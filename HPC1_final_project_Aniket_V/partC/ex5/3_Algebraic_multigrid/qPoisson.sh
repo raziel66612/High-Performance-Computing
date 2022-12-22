db90caf8d6a6bfff10e2eabd7d6f8541714cf622
Aniket R Vishwakarma <103066248+raziel66612@users.noreply.github.com>
2022-12-21 23:57:07 -0500

    Add files via upload

---
+++ HPC1_final_project_Aniket_V/partC/ex5/3_Algebraic_multigrid/qPoisson.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=hpc1
+#SBATCH --partition=general-compute
+#SBATCH --qos=general-compute
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=00:10:00
+#SBATCH --constraint=IB&CPU-Gold-6230
+#SBATCH --job-name="Poisson_ex5_"
+#SBATCH --output=Poisson_ex5_-%j.out
+#SBATCH --error=Poisson_ex5_-%j.err
+
+
+module load intel/17.0
+module load intel-mpi/2017.0.1
+export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
+
+module load petsc/v3.15.0
+
+for i in {128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,256,512,1024,2048};
+do srun ./ex5 -pc_type gamg -da_grid_x $i -da_grid_y $i -mms 3 -log_view > grid_$i.txt;
+done
+
+
+echo "DONE !!!!"
